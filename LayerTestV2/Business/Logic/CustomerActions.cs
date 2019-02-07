f9ba9d98c55987994f408dcf252763f33612c75e
Osku Sirpoma <osku.sirpoma@gmail.com>
2019-02-07 14:10:46 +0200

    new project

---
+++ LayerTestV2/Business/Logic/CustomerActions.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.Database.AppDbContext;
+using DataAccess.Database.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business.Logic
+{
+    public class CustomerActions
+    {
+        public ApplicationDbContext db = new ApplicationDbContext();
+
+        public Customer GetCustomer(string un, string pw)
+        {
+            var user = db.Customer
+                .Where(c => c.Username == un & c.Password == pw) as Customer;
+            return user;
+        }
+
+        public List<Customer> getAllCustomers()
+        {
+            var user = db.Customer.ToList();
+            return user;
+        }
+
+        public void addCustomer(Customer customer)
+        {
+            db.Customer.Add(customer);
+            db.SaveChanges();
+        }
+    }
+}
