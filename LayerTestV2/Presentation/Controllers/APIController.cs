917df048a7877f4e0103cb66a4856d51da6e7e8f
Osku Sirpoma <osku.sirpoma@gmail.com>
2019-02-08 13:15:14 +0200

    -

--- LayerTestV2/Presentation/Controllers/APIController.cs
+++ LayerTestV2/Presentation/Controllers/APIController.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Logic;
+using DataAccess.Database.Table;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class APIController : ControllerBase
     {
+
+        CustomerActions CA = new CustomerActions();
+
+        [HttpGet("[action]")]
+        public List<Customer> index()
+        {
+            return CA.getAllCustomers();
+        }
     }
 }
