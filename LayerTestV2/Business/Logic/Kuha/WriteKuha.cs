31befe862e0bc9eb4315bd839ad0a2ea96178599
OsKuZz <osku.sirpoma@gmail.com>
2019-02-08 16:04:26 +0200

    kuha ei ole koira

---
+++ LayerTestV2/Business/Logic/Kuha/WriteKuha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business.Logic.Log
+{
+    public class WriteLog
+    {
+        public void writeLog(string logdata)
+        {
+            System.IO.StreamWriter("C:/Users/osku0/Desktop/Projects/Git/LayerTest/LayerTestV2/DataAccess/Log/Logs/newCustomerLog.txt", logdata + "\n");
+            //C:\Program Files\Log\Logs\newCustomerLog.txt
+        }
+    }
+}
