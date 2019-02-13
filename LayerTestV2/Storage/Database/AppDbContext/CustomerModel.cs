83e5fcfecfba3eae2d921ecf2bd0fc41cc7e83dc
Bulbakin <julius.kulju@hotmail.fi>
2019-02-13 13:35:15 +0200

    -

--- LayerTestV2/Storage/Database/AppDbContext/CustomerModel.cs
+++ LayerTestV2/Storage/Database/AppDbContext/CustomerModel.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Storage.Log
+namespace Storage.Database.AppDbContext
 {
     public class CustomerModel : IEntityTypeConfiguration<Customer>
     {
